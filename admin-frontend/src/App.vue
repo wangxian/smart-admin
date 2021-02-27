0d501daad7627fe4d0ef70e95d8cc80de3063582
wangxian <xian366@126.com>
2021-02-28 00:08:01 +0800

    修改：前端，代码整理及格式化

--- admin-frontend/src/App.vue
+++ admin-frontend/src/App.vue
@@ -1,31 +1,34 @@
-
 <template>
   <div id="app">
-    <router-view />
+    <router-view/>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App',
-  created() {}
-};
+  export default {
+    name: 'App',
+    created () {
+    }
+  };
 </script>
 
 <style lang="less">
-@import './main.less';
-.size {
-  width: 100%;
-  height: 100%;
-}
-html,
-body {
-  .size;
-  overflow: hidden;
-  margin: 0;
-  padding: 0;
-}
-#app {
-  .size;
-}
+  @import './main.less';
+
+  .size {
+    width: 100%;
+    height: 100%;
+  }
+
+  html,
+  body {
+    .size;
+    overflow: hidden;
+    margin: 0;
+    padding: 0;
+  }
+
+  #app {
+    .size;
+  }
 </style>
--- admin-frontend/src/main.js
+++ admin-frontend/src/main.js
@@ -17,24 +17,25 @@ import './themes/index.less';
 import '@/assets/icons/iconfont.css';
 import 'slick-carousel/slick/slick.css';
 import { Decimal } from 'decimal.js';
+
 // 枚举管理
 import Enum from 'vue-enum';
 import enumInfo from '@/constants';
+
 // 处理table操作按钮
 import tableAction from './lib/table-action';
 
-//时间
+// 时间
 import moment from 'moment';
-
 
 Vue.prototype.$tableAction = tableAction;
 Vue.use(Enum, { enumInfo });
 Vue.use(ViewUI, {
   i18n: (key, value) => i18n.t(key, value)
 });
+
 Vue.use(JsonViewer);
 Vue.use(vClickOutside);
-
 
 Number.prototype.toFixed = function (length) {
   let x = new Decimal(this);
@@ -45,19 +46,21 @@ Number.prototype.toFixed = function (length) {
 moment.locale('zh-cn'); // 设置语言 或 moment.lang('zh-cn');
 Vue.prototype.$moment = moment;// 赋值使用
 
-
 /**
  * @description 注册admin内置插件
  */
 installPlugin(Vue);
+
 /**
  * @description 生产环境关掉提示
  */
 Vue.config.productionTip = false;
+
 /**
  * @description 全局注册应用配置
  */
 Vue.prototype.$config = config;
+
 /**
  * 注册指令
  */
